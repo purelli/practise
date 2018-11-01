0d6a7de31fcc4e8f52a381e11ca4bc601bcb81dd
Priya- PC <Priya- PC@DESKTOP-HUGL9US>
2018-11-01 11:52:10 -0400

    bug fixed

---
+++ Application/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Application")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Application")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("efdb26c6-7bf8-41c6-ae0c-099102d2f796")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Application/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Application
+{
+    
+    public class UnitTest1
+
+    {
+        static void Main()
+        {
+            SpecialElements();
+        }
+        static void SpecialElements()
+        {
+            IWebDriver driver = new ChromeDriver();
+            string url = "http://testing.todvachev.com/";
+
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+        
+            try
+            {
+                IWebElement csselement = driver.FindElement(By.Id("menu-item-35"));
+                csselement.Click();
+
+                //Thread.Sleep(5000);
+               //clicking on alert
+                IWebElement alertbox = driver.FindElement(By.CssSelector("#main-content > article.mh-loop-item.clearfix.post-119.post.type-post.status-publish.format-standard.has-post-thumbnail.hentry.category-special-elements > div.mh-loop-content.clearfix > header > h3 > a"));
+                Thread.Sleep(5000);
+                
+                IAlert alert = driver.SwitchTo().Alert();
+                alert.Accept();//clicks on okay
+
+                Thread.Sleep(5000);
+                driver.Navigate().Back();//goes back to special elements page
+
+                driver.Quit();
+              }
+
+            catch (NoSuchElementException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+   
+    }
+}
